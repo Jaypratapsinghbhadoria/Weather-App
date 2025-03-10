9f9f59181aa9fc57bc6d021e5462f45e9df4a0d0
Jay Pratap Singh Bhadoria <147808554+Jaypratapsinghbhadoria@users.noreply.github.com>
2025-03-10 22:36:22 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -10,7 +10,7 @@ const error = document.querySelector(".oops")
 const weather_body = document.querySelector(".weather_body")
 
 async function checkWeather(city) {
-    const api_key = "API_KEY";
+    const api_key = 64a50d98c543deaa3972d08a76c4a050;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
